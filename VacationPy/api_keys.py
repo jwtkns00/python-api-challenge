27be821c7631ee8ecdff25bbbbe1ea696a31ef02
Joel Watkins <jw@Joels-MacBook-Pro.local>
2020-10-19 15:43:29 -0700

    added files to .gitignore

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "2038a204ccb61acb95238504e9666081"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDmTFT188cZB5__OKE6S4Qq_d315bHt2mw"
